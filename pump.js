6f646ea48f3f5dca317c4059ddf3c8cf981a3701
Will <will.pittman@comcast.net>
2016-04-18 18:25:32 -0400

    Basically working

--- pump.js
+++ pump.js
@@ -3,7 +3,10 @@
 var CLIENT_ID = '725282832720-s4ojhj9109me82efof9ckspjv90vqpuv.apps.googleusercontent.com';
 
 var SCOPES = ['https://www.googleapis.com/auth/drive',
-              'https://www.googleapis.com/auth/spreadsheets',];
+              'https://www.googleapis.com/auth/spreadsheets',
+              'https://www.googleapis.com/auth/userinfo.email',];
+
+var SCRIPT_ID = "M0rgzSfuM9Euxa01JgFmJ4OyauSKwSQOx";
 
 /**
  * Check if current user has authorized this application.
@@ -27,7 +30,8 @@ function handleAuthResult(authResult) {
   if (authResult && !authResult.error) {
     // Hide auth UI, then load client library.
     authorizeDiv.style.display = 'none';
-    callScriptFunction();
+    //callScriptFunction();
+    //callPumpoutFunction();
   } else {
     // Show auth UI, allowing the user to initiate authorization by
     // clicking authorize button.
@@ -47,12 +51,75 @@ function handleAuthClick(event) {
   return false;
 }
 
+function testFunc() {
+    console.log("its working");
+}
+
+function callPumpoutFunction() {
+
+      // Create an execution request object.
+
+      var data = document.getElementsByName("data");
+
+      var request = {
+          'function': 'appendPumpout',
+          'parameters': [
+              data[0].value,
+              data[1].value,
+              data[2].value,
+              data[3].value,
+              data[4].value,
+              data[5].value,
+              data[6].value,
+          ],
+          'devMode' : true,
+          };
+
+      // Make the API request.
+      var op = gapi.client.request({
+          'root': 'https://script.googleapis.com',
+          'path': 'v1/scripts/' + SCRIPT_ID + ':run',
+          'method': 'POST',
+          'body': request
+      });
+
+      op.execute(function(resp) {
+        if (resp.error && resp.error.status) {
+          // The API encountered a problem before the script
+          // started executing.
+          appendPre('Error calling API:');
+          appendPre(JSON.stringify(resp, null, 2));
+        } else if (resp.error) {
+          // The API executed, but the script returned an error.
+
+          // Extract the first (and only) set of error details.
+          // The values of this object are the script's 'errorMessage' and
+          // 'errorType', and an array of stack trace elements.
+          var error = resp.error.details[0];
+          appendPre('Script error message: ' + error.errorMessage);
+
+          if (error.scriptStackTraceElements) {
+            // There may not be a stacktrace if the script didn't start
+            // executing.
+            appendPre('Script error stacktrace:');
+            for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+              var trace = error.scriptStackTraceElements[i];
+              appendPre('\t' + trace.function + ':' + trace.lineNumber);
+            }
+          }
+        } else {
+
+          var folderSet = resp.response.result;
+          appendPre(folderSet);
+        }
+      });
+}
+
 /**
  * Calls an Apps Script function to list the folders in the user's
  * root Drive folder.
  */
 function callScriptFunction() {
-  var scriptId = "M0rgzSfuM9Euxa01JgFmJ4OyauSKwSQOx";
 
   // Create an execution request object.
   var request = {
@@ -63,7 +130,7 @@ function callScriptFunction() {
   // Make the API request.
   var op = gapi.client.request({
       'root': 'https://script.googleapis.com',
-      'path': 'v1/scripts/' + scriptId + ':run',
+      'path': 'v1/scripts/' + SCRIPT_ID + ':run',
       'method': 'POST',
       'body': request
   });
