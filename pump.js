a80f66098f8e9c225f7125540a0b463b2e58fa97
Will <will.pittman@comcast.net>
2016-04-22 18:43:33 -0400

    all basic functions working

--- pump.js
+++ pump.js
@@ -1,5 +1,3 @@
-// Your Client ID can be retrieved from your project in the Google
-// Developer Console, https://console.developers.google.com
 var CLIENT_ID = "362878149208-dfs57d9rsl70f9bet7f5a55mu5rfhp6u.apps.googleusercontent.com";
 
 var SCOPES = ["https://www.googleapis.com/auth/plus.login",
@@ -8,9 +6,9 @@ var SCOPES = ["https://www.googleapis.com/auth/plus.login",
 
 var SCRIPT_ID = "M6VwDp8tq5aObkqmMh7xb8ZSCr23GME13";
 
-var SAVE_RANGE = -1; // normally 10
+var SAVE_RANGE = -1; // normally 10, -1 is all of the data
 
-var savednames = [];
+var DEBUG = true;
 
 /**
 * Check if current user has authorized this application.
@@ -23,138 +21,19 @@ function checkAuth() {
     }, handleAuthResult);
 }
 
-function fired() {
-    console.log("fired");
-    $("#ddmenu").append("<li><a onclick=\"fired()\">Savage</a></li>");
-}
-
-function autofill(id) {
-    var request = {
-        'function': 'getDataFromIndex',
-        'parameters': [
-            id,
-            SAVE_RANGE,
-        ],
-        //'devMode' : true,
-    };
-
-    // Make the API request.
-    var op = gapi.client.request({
-        'root': 'https://script.googleapis.com',
-        'path': 'v1/scripts/' + SCRIPT_ID + ':run',
-        'method': 'POST',
-        'body': request
-    });
-
-    op.execute(function(resp) {
-        if (resp.error && resp.error.status) {
-            // The API encountered a problem before the script
-            // started executing.
-            appendOutputDiv('Error calling API:');
-            appendOutputDiv(JSON.stringify(resp, null, 2));
-        } else if (resp.error) {
-            // The API executed, but the script returned an error.
-
-            // Extract the first (and only) set of error details.
-            // The values of this object are the script's 'errorMessage' and
-            // 'errorType', and an array of stack trace elements.
-            var error = resp.error.details[0];
-            appendOutputDiv('Script error message: ' + error.errorMessage);
-
-            if (error.scriptStackTraceElements) {
-                // There may not be a stacktrace if the script didn't start
-                // executing.
-                appendOutputDiv('Script error stacktrace:');
-                for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-                    var trace = error.scriptStackTraceElements[i];
-                    appendOutputDiv('\t' + trace.function + ':' + trace.lineNumber);
-                }
-            }
-        } else {
-            var response = resp.response.result;
-
-            var field = document.getElementsByName("data");
-
-            arraymove(savednames, id, savednames.length-1);
-
-            $("#ddmenu").empty();
-
-            for(var i=0; i < savednames.length; i++) {
-                $("#ddmenu").append("<li><a onclick=\"autofill(" + i + ")\">" + savednames[i] + "</a></li>");
-            }
-
-            for(var i=0; i < response.length; i++) {
-                field[i].value = response[i];
-            }
-        }
-    });
-}
-
-function arraymove(arr, fromIndex, toIndex) {
-    var element = arr[fromIndex];
-    arr.splice(fromIndex, 1);
-    arr.splice(toIndex, 0, element);
-}
-
-function populateDropdown() {
-    var request = {
-        'function': 'getRecent',
-        'parameters': [
-            SAVE_RANGE,
-        ],
-        //'devMode' : true,
-    };
-
-    // Make the API request.
-    var op = gapi.client.request({
-        'root': 'https://script.googleapis.com',
-        'path': 'v1/scripts/' + SCRIPT_ID + ':run',
-        'method': 'POST',
-        'body': request
-    });
-
-    op.execute(function(resp) {
-        if (resp.error && resp.error.status) {
-            // The API encountered a problem before the script
-            // started executing.
-            appendOutputDiv('Error calling API:');
-            appendOutputDiv(JSON.stringify(resp, null, 2));
-        } else if (resp.error) {
-            // The API executed, but the script returned an error.
-
-            // Extract the first (and only) set of error details.
-            // The values of this object are the script's 'errorMessage' and
-            // 'errorType', and an array of stack trace elements.
-            var error = resp.error.details[0];
-            appendOutputDiv('Script error message: ' + error.errorMessage);
-
-            if (error.scriptStackTraceElements) {
-                // There may not be a stacktrace if the script didn't start
-                // executing.
-                appendOutputDiv('Script error stacktrace:');
-                for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-                    var trace = error.scriptStackTraceElements[i];
-                    appendOutputDiv('\t' + trace.function + ':' + trace.lineNumber);
-                }
-            }
-        } else {
-
-            var response = resp.response.result;
-
-            savednames = response;
-
-            for(var i=0; i < response.length; i++) {
-                $("#ddmenu").append("<li><a onclick=\"autofill(" + i + ")\">" + response[i] + "</a></li>");
-            }
-            appendOutputDiv("names loaded");
-        }
-    });
+/**
+* Initiate auth flow in response to user clicking authorize button.
+*/
+function handleAuthClick(event) {
+    gapi.auth.authorize({
+        client_id: CLIENT_ID, scope: SCOPES, immediate: false
+    },
+    handleAuthResult);
+    return false;
 }
 
 /**
 * Handle response from authorization server.
-*
-* @param {Object} authResult Authorization result.
 */
 function handleAuthResult(authResult) {
     var authorizeDiv = document.getElementById("authorize-div");
@@ -171,17 +50,64 @@ function handleAuthResult(authResult) {
     }
 }
 
-/**
-* Initiate auth flow in response to user clicking authorize button.
-*
-* @param {Event} event Button click event.
-*/
-function handleAuthClick(event) {
-    gapi.auth.authorize({
-        client_id: CLIENT_ID, scope: SCOPES, immediate: false
-    },
-    handleAuthResult);
-    return false;
+function autofill(id) {
+    var request = {
+        'function': 'getDataFromIndex',
+        'parameters': [
+            id,
+            SAVE_RANGE,
+        ],
+        'devMode' : DEBUG,
+    };
+
+    // Make the API request.
+    var op = gapi.client.request({
+        'root': 'https://script.googleapis.com',
+        'path': 'v1/scripts/' + SCRIPT_ID + ':run',
+        'method': 'POST',
+        'body': request
+    });
+
+    op.execute(function(resp) {
+        if (!parseAPIResponse(resp)) {
+            var response = resp.response.result;
+
+            var field = document.getElementsByName("data");
+
+            for(var i=0; i < response.length; i++) {
+                field[i].value = response[i];
+            }
+        }
+    });
+}
+
+function populateDropdown() {
+    var request = {
+        'function': 'getRecent',
+        'parameters': [
+            SAVE_RANGE,
+        ],
+        'devMode' : DEBUG,
+    };
+
+    // Make the API request.
+    var op = gapi.client.request({
+        'root': 'https://script.googleapis.com',
+        'path': 'v1/scripts/' + SCRIPT_ID + ':run',
+        'method': 'POST',
+        'body': request
+    });
+
+    op.execute(function(resp) {
+        if (!parseAPIResponse(resp)) {
+            var response = resp.response.result;
+
+            for(var i=0; i < response.length; i++) {
+                $("#ddmenu").append("<li><a onclick=\"autofill(" + i + ")\">" + response[i] + "</a></li>");
+            }
+            appendOutputDiv("names loaded");
+        }
+    });
 }
 
 function callPumpoutFunction() {
@@ -199,7 +125,7 @@ function callPumpoutFunction() {
             data[5].value,
             data[6].value,
         ],
-        //'devMode' : true,
+        'devMode' : DEBUG,
     };
 
     // Make the API request.
@@ -211,46 +137,53 @@ function callPumpoutFunction() {
     });
 
     op.execute(function(resp) {
-        if (resp.error && resp.error.status) {
-            // The API encountered a problem before the script
-            // started executing.
-            appendOutputDiv('Error calling API:');
-            appendOutputDiv(JSON.stringify(resp, null, 2));
-        } else if (resp.error) {
-            // The API executed, but the script returned an error.
-
-            // Extract the first (and only) set of error details.
-            // The values of this object are the script's 'errorMessage' and
-            // 'errorType', and an array of stack trace elements.
-            var error = resp.error.details[0];
-            appendOutputDiv('Script error message: ' + error.errorMessage);
-
-            if (error.scriptStackTraceElements) {
-                // There may not be a stacktrace if the script didn't start
-                // executing.
-                appendOutputDiv('Script error stacktrace:');
-                for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-                    var trace = error.scriptStackTraceElements[i];
-                    appendOutputDiv('\t' + trace.function + ':' + trace.lineNumber);
-                }
-            }
-        } else {
-
-            var folderSet = resp.response.result;
-            appendOutputDiv(folderSet);
+        if(!parseAPIResponse(resp)) {
+            var post_stats = resp.response.result;
+            appendOutputDiv(post_stats);
         }
     });
+}
+
+function parseAPIResponse(resp) {
+    if (resp.error && resp.error.status) {
+        // The API encountered a problem before the script
+        // started executing.
+        appendOutputDiv('Error calling API:');
+        appendOutputDiv(JSON.stringify(resp, null, 2));
+        return true;
+    } else if (resp.error) {
+        // The API executed, but the script returned an error.
+
+        // Extract the first (and only) set of error details.
+        // The values of this object are the script's 'errorMessage' and
+        // 'errorType', and an array of stack trace elements.
+        var error = resp.error.details[0];
+        appendOutputDiv('Script error message: ' + error.errorMessage);
+
+        if (error.scriptStackTraceElements) {
+            // There may not be a stacktrace if the script didn't start
+            // executing.
+            appendOutputDiv('Script error stacktrace:');
+            for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+                var trace = error.scriptStackTraceElements[i];
+                appendOutputDiv('\t' + trace.function + ':' + trace.lineNumber);
+            }
+        }
+        return true;
+    }
+    return false;
 }
 
 /**
 * Append a pre element to the body containing the given message
 * as its text node.
-*
-* @param {string} message Text to be placed in pre element.
 */
 function appendOutputDiv(message) {
     var pre = document.getElementById('output');
     var textContent = document.createTextNode(message + '\n');
-    //pre.appendChild(textContent);
-    pre.textContent = message + "\n";
+    if(DEBUG) {
+        pre.appendChild(textContent);
+    } else {
+        //pre.textContent = message + "\n";
+    }
 }
